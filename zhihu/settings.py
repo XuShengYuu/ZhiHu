523dcf3dca01905cd3e7f60dd81eb8bfd97f1eb2
XuShengYuu <45997837+XuShengYuu@users.noreply.github.com>
2019-05-22 17:14:38 +0800

    a

--- zhihu/settings.py
+++ zhihu/settings.py
@@ -97,7 +97,7 @@ MONGO_DATABASE = 'zhihu'
 SCHEDULER = "scrapy_redis.scheduler.Scheduler"
 
 DUPEFILTER_CLASS = "scrapy_redis.dupefilter.RFPDupeFilter"
-REDIS_URL = 'redis://:123456@192.168.31.46:6379'
+REDIS_URL = 'redis://:123456@113.78.185.154:6379'
 
 # REDIS_HOST = '192.168.31.46'
 # REDIS_PORT = 6379
